0decd0a1273f1e7ac9c5662f877194b75c97c527
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2022-12-17 11:22:18 +0100

    Update install.py

--- dockerbot/install.py
+++ dockerbot/install.py
@@ -51,7 +51,7 @@ def install():
         print(f"{bcolors.WARNING}70% Done{bcolors.ENDC}")
         os.system("git clone https://github.com/dxomg/vpsbot")
         os.system("cd vpsbot/dockerbot")
-        os.system("wget -O imagesamd64.tar.gz 'https://drive.google.com/uc?id=1iUUNLS4hhSk6vCXMwjR09jCOFAblK7as&export=download&confirm=t&uuid=6aa2ceab-5731-4e33-ab08-f054aedafe3e' && docker load < imagesamd64.tar")
+        os.system("wget -O imagesamd64.tar.gz 'https://drive.google.com/uc?id=1iUUNLS4hhSk6vCXMwjR09jCOFAblK7as&export=download&confirm=t&uuid=6aa2ceab-5731-4e33-ab08-f054aedafe3e' && docker load < imagesamd64.tar.gz")
         print(f"{bcolors.WARNING}100% Done{bcolors.ENDC}")
     if architecture  == 2:
         print(f"{bcolors.WARNING}Installing requirements{bcolors.ENDC}")
@@ -59,7 +59,7 @@ def install():
         print(f"{bcolors.WARNING}70% Done{bcolors.ENDC}")
         os.system("git clone https://github.com/dxomg/vpsbot")
         os.system("cd vpsbot/dockerbot")
-        os.system("wget -O imagesamd64.tar.gz 'https://drive.google.com/uc?id=1lGrbGAnDBbGpUH63X_zq2Bxy0EeOXNnY&export=download&confirm=t&uuid=90ee1fa3-806d-40c3-9cc1-d31adf586b17' && docker load < imagesarm64.tar")
+        os.system("wget -O imagesamd64.tar.gz 'https://drive.google.com/uc?id=1lGrbGAnDBbGpUH63X_zq2Bxy0EeOXNnY&export=download&confirm=t&uuid=90ee1fa3-806d-40c3-9cc1-d31adf586b17' && docker load < imagesarm64.tar.gz")
         print(f"{bcolors.OKGREEN}100% Done{bcolors.ENDC}")
         print(f"""
         {bcolors.WARNING}Install has been completed!, now go edit the config.cfg file to your needs
